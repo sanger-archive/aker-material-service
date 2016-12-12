2998f036b694aedee9fa506c3f3a71b618144b3d
Chris Smith <cs24@sanger.ac.uk>
2016-12-12 16:03:06 +0000

    Removed RESOURCE DELETE
    You now can not DELETE all materials in one request

--- schema.py
+++ schema.py
@@ -1,7 +1,7 @@
 # Enable reads (GET), inserts (POST) and DELETE for resources/collections
 # (if you omit this line, the API will default to ['GET'] and provide
 # read-only access to the endpoint).
-RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+RESOURCE_METHODS = ['GET', 'POST']
 
 # Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
 # individual items  (defaults to read-only item access).
