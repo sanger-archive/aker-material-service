f28f8c7f85f2373f244ee404be937e156d702c5c
Chris Smith <cs24@sanger.ac.uk>
2017-02-22 16:53:46 +0000

    Added swagger ui. Available from /docs (points at /api-docs)

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ from custom_validator import CustomValidator
 from eve import Eve
 from flask import request, jsonify, abort, Response, current_app
 from eve_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
 from bson import json_util
 from flask_zipkin import Zipkin
 from pymongo import ReturnDocument
@@ -18,6 +19,8 @@ from addresser import Addresser
 environment = os.getenv('EVE_ENV', 'development')
 
 SETTINGS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'db', environment + '.py')
+SWAGGER_URL = '/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/api-docs'  # Our API url (can of course be a local resource)
 
 def create_app(settings):
   app = Eve(settings=settings, json_encoder=UUIDEncoder, validator=CustomValidator)
@@ -29,7 +32,11 @@ def create_app(settings):
       .get_collection('counters') \
       .update({'_id': 'barcode'}, {'$setOnInsert': {'seq': 0}}, upsert=True)
 
+  # Create a swagger.json
   app.register_blueprint(swagger)
+
+  # Configure swagger ui to display docs using swagger.json @ SWAGGER_URL
+  app.register_blueprint(get_swaggerui_blueprint(SWAGGER_URL, API_URL), url_prefix=SWAGGER_URL)
 
   def set_uuid(resource_name, items):
     for item in items:
